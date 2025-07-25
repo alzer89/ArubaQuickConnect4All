2ab8fea32de2696d6756096eed88f7d883cc595c
alzer89 <alexjlong@web.de>
2025-07-26 00:00:11 +1000

    Adding missing regex

--- src/aqc4all/main.py
+++ src/aqc4all/main.py
@@ -41,6 +41,13 @@ def parse_args():
 def check_for_required_fields(args):
     import getpass
     global USERNAME, PASSWORD, BASE_URL, TOTP_SECRET, BROWSER
+    regex = re.compile(
+            r'^(?:http|ftp)s?://' # http:// or https://
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+            r'localhost|' #localhost...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+            r'(?::\d+)?' # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
     USERNAME = args.username
     while not USERNAME:
         USERNAME = input('Enter your username (REQUIRED): ')
